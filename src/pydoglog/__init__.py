b8b2d926770d79607c0f8123b1a78c1d84c5f5a0
prestomation <prestomation@users.noreply.github.com>
2026-03-16 04:10:29 +0000

    chore: bump version to 0.1.1

--- src/pydoglog/__init__.py
+++ src/pydoglog/__init__.py
@@ -5,7 +5,7 @@ from .client import DogLogClient
 from .exceptions import DogLogAPIError, DogLogAuthError, DogLogError, DogLogNotFoundError
 from .models import Dog, DogEvent, EventType, Pack
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "AsyncDogLogClient",
